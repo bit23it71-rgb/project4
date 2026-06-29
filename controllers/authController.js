8875b1e96ae1e06688662c6a3f33547f8e2803f0
adarsh <bit23it71@bit.ac.in>
2026-06-29 11:56:39 +0530

    SEcond

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,21 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs"); 
 
-const register = ()=>{
+const register = async(req,res)=>{
     try{
+     const {name,email,password} = req.body;
+     const oldUser = await User.findOne({email});
+     if(oldUser){
+        return res.Status(400).json({
+            success:false,
+            message:"User already exist"
+        });
+     }
+      const hashPassword = await bcrypt.hash(password,10);
+
+      const user = await User.create({name,email,password:hashPassword});
+
+      res.status(201).json
 
     }
 
