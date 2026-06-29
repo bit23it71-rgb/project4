a79768b2fbea84f80bcdf0f74599b444d2c0fcb2
adarsh <bit23it71@bit.ac.in>
2026-06-29 10:37:42 +0530

    first commit

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+const mongoose= require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    tittle:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{
+ timestamps:true
+});
+
+module.exports= mongoose.model("Post", postSchema);
